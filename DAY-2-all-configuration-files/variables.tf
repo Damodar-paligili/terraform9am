83122b14fce24f2922d59acead32bae400192eb8
Damodar-Paligili <damodarpp99@gmail.com>
2026-06-02 16:35:47 +0530

    day-3

--- DAY-2-all-configuration-files/variables.tf
+++ DAY-2-all-configuration-files/variables.tf
@@ -1,12 +1,8 @@
 variable "ami_id" {
-    description = "value"
-    default = "ami-00e801948462f718a"
 }
 
 variable "instance_type" {
-  default = "t2.medium"
 }
 
 variable "Name" {
-  default = "my-instance"
 }
